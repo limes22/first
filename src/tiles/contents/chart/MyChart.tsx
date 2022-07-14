65ee08b2384521866a79b98995d80b9a924b1569
limes22 <howdi2002@naver.com>
2022-07-14 04:32:28 +0000

    table

--- src/tiles/contents/chart/MyChart.tsx
+++ src/tiles/contents/chart/MyChart.tsx
@@ -12,18 +12,45 @@ interface MyChartProps {
 const MyChart = ({ viewChart }: MyChartProps) => {
   return (
     <>
-      <h1>Practice</h1>
-        <h2>제목</h2>
-        {
-            viewChart.map((element: any, idx: number) => (
-                <h2
-                >
-                {element.id}
-                {element.title}
-                {element.body}
-                </h2>
-            ))
-        }
+    							{/* <!-- Pie chart starts --> */}
+
+<div className="widget wviolet">
+
+<div className="widget-head">
+  <div className="pull-left">Pie Chart</div>
+  <div className="widget-icons pull-right">
+  <a href="#" className="wminimize"><i className="fa fa-chevron-up"></i></a> 
+  <a href="#" className="wclose"><i className="fa fa-times"></i></a>
+  </div>
+  <div className="clearfix"></div>
+</div>
+
+
+<div className="widget-content">
+										<div className="table-responsive">
+                      {viewChart.map((element: any, idx: number)=> (
+                        <h2>
+											<table className="table table-bordered ">
+												{/* <thead>
+													<tr>
+                          {element.id}
+													</tr>
+												</thead> */}
+												<tbody>
+													<tr>
+													  <td>{element.id}</td>
+													  <td>{element.title}</td>
+													  <td>{element.body}</td>
+													</tr>                                                                 
+												</tbody>
+											</table>
+                        </h2>
+                      ))}
+
+										</div>
+									</div>
+</div>
+{/* <!-- Pie chart ends --> */}     
     </>
   );
       }
