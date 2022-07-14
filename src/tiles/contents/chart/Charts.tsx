c0e1f271a8c03592a5c4c3aac9eef526532fd1dc
limes22 <howdi2002@naver.com>
2022-07-14 04:52:35 +0000

    3

--- src/tiles/contents/chart/Charts.tsx
+++ src/tiles/contents/chart/Charts.tsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "../../../css/bootstrap.min.css";
 import "../../../css/jquery-ui.css";
 import "../../../css/font-awesome.min.css";
 import "../../../css/style.css";
 import "../../../css/widgets.css";
 import MyChart from "./MyChart";
-import axios from "axios";
 import { data } from "jquery";
 
 const Charts = () => {
-  const [viewChart, setViewChart] = useState<any>([
-  ]);
-
-  const getPosts = async () => {
-    const res = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts?userId=1"
-    );
-    setViewChart(res.data);
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  console.log(viewChart);
+  
 
   return (
     <>
@@ -60,7 +44,7 @@ const Charts = () => {
             <div className="container">
               <div className="row">
                 <div className="col-md-12">
-                  <MyChart viewChart={viewChart} />
+                  <MyChart />
                 </div>
               </div>
             </div>
--- src/tiles/contents/chart/MyChart.tsx
+++ src/tiles/contents/chart/MyChart.tsx
@@ -1,60 +1,74 @@
 import React, { Fragment } from "react";
+import { useState, useEffect } from "react";
 import "../../../css/bootstrap.min.css";
 import "../../../css/jquery-ui.css";
 import "../../../css/font-awesome.min.css";
 import "../../../css/style.css";
 import "../../../css/widgets.css";
+import axios from "axios";
 
-interface MyChartProps {
-  viewChart: any;
-}
 
-const MyChart = ({ viewChart }: MyChartProps) => {
+const MyChart = () => {
+  const [viewChart, setViewChart] = useState<any>([
+  ]);
+
+  async function getPosts() {
+    const res = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts?userId=1"
+    );
+    setViewChart(res.data);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  console.log(viewChart);
   return (
     <>
-    							{/* <!-- Pie chart starts --> */}
+      {/* <!-- Pie chart starts --> */}
 
-<div className="widget wviolet">
+      <div className="widget wviolet">
 
-<div className="widget-head">
-  <div className="pull-left">Pie Chart</div>
-  <div className="widget-icons pull-right">
-  <a href="#" className="wminimize"><i className="fa fa-chevron-up"></i></a> 
-  <a href="#" className="wclose"><i className="fa fa-times"></i></a>
-  </div>
-  <div className="clearfix"></div>
-</div>
+        <div className="widget-head">
+          <div className="pull-left">Pie Chart</div>
+          <div className="widget-icons pull-right">
+            <a href="#" className="wminimize"><i className="fa fa-chevron-up"></i></a>
+            <a href="#" className="wclose"><i className="fa fa-times"></i></a>
+          </div>
+          <div className="clearfix"></div>
+        </div>
 
 
-<div className="widget-content">
-										<div className="table-responsive">
-                      
-											<table className="table table-bordered ">
-												<thead>
-													<tr>
-                          <td>id</td>
-                          <td>title</td>
-                          <td>body</td>
-													</tr>
-												</thead>
-                        {viewChart.map((element: any, idx: number)=> (
-												<tbody>
-													<tr>
-													  <td>{element.id}</td>
-													  <td>{element.title}</td>
-													  <td>{element.body}</td>
-													</tr>                                                                 
-												</tbody>
-                        ))}
-											</table>                   
-                      
+        <div className="widget-content">
+          <div className="table-responsive">
 
-										</div>
-									</div>
-</div>
-{/* <!-- Pie chart ends --> */}     
+            <table className="table table-bordered ">
+              <thead>
+                <tr>
+                  <td>id</td>
+                  <td>title</td>
+                  <td>body</td>
+                </tr>
+              </thead>
+              {viewChart.map((element: any, idx: number) => (
+                <tbody>
+                  <tr>
+                    <td>{element.id}</td>
+                    <td>{element.title}</td>
+                    <td>{element.body}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+
+
+          </div>
+        </div>
+      </div>
+      {/* <!-- Pie chart ends --> */}
     </>
   );
-      }
+}
 
 export default MyChart;
